38b67d27af808e10792a22bec8c01190bf75527e
simonaturco <simonaturco88@gmail.com>
2025-10-15 14:05:11 +0200

    testing starting from scratch

--- images/registration_gif_processing.m
+++
@@ -1,9 +0,0 @@
-%Open GIF and shorten it
-filename = 'C:\Users\sturco\Work_local\Personal Page\simonaturco.github.io\images\registration.gif';
-[imgs,map]=imread(filename,'frames','all');
-
-for i=1:size(imgs,4)
-    frame = rgb2ind(squeeze(imgs(:,:,:,i)));
-    imshow(frame)
-    pause(0.5)
-end
